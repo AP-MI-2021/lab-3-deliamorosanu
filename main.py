84b97e2d31ff82cf806eb04e28cc009ddd231294
deliamorosanu <dede.morosanu2016@gmail.com>
2021-10-13 19:51:56 +0300

    lab modificat

--- main.py
+++ main.py
@@ -11,32 +11,39 @@ def citiredate():
 7. Toate nu sunt prime.
 '''
 
-
-def not_prime(nr):
-    if nr < 2:
-        return True
-    if nr ==2 :
+def is_prime(n) -> bool:
+    '''
+    Determina primalitatea lui n
+    :param n: nr natural
+    :return: True daca n este prim, altfel False
+    '''
+    if int(n) < 2:
         return False
-    for i in range(1, len(lst)):
-        for d in range(3, len(lst)//2):
-            if lst[i] % d == 0:
-                return True
-        return False
-    pass
+    for i in range(2, int(n) // 2 + 1):
+        if int(n) % int(i) == 0: return False
+    return True
+
+def verify_all_are_prime(lst: list[int]) -> bool:
+    for nr in lst:
+        if is_prime(nr) == False:
+            return False
+    return True
 
 
-def get_longest_all_not_prime(lst: list[int]):
-    submax = []
+def get_longest_all_prime(lst: list[int]) -> list[int]:
+    result = []
     for i in range(len(lst)):
         for j in range(i, len(lst)):
-            if not_prime(lst[i: j + 1]) and len(lst[i: j + 1]) > len(submax):
-                submax = lst[i: j + 1]
-    return submax
+            if verify_all_are_prime(lst[i:j + 1]) and len(lst[i:j + 1]) > len(result):
+                result = lst[i:j + 1]
+    return result
 
 
-def test_get_longest_not_all_prime():
-    assert get_longest_all_not_prime([4,6,8,10,11,13]) == [4,6,8,10]
-    assert get_longest_all_not_prime([3,5,7,11,13,14]) == [14]
+def test_get_longest_all_prime():
+    assert get_longest_all_prime([1,2,3,4,5]) == [2,3]
+    assert get_longest_all_prime([1,6,2,3,5,7,11]) == [2,3,5,7,11]
+    assert get_longest_all_prime([6,9,10,7,6,49,56]) == [7]
+
 
 
 def arithmetic_progression(lst: list[int]):
@@ -61,36 +68,52 @@ def get_longest_arithmetic_progression(lst: list[int]):
     return subprogarit
 
 
-def test_get_longest_arithmetic_progression():
-    assert get_longest_arithmetic_progression([1,4,5,8,11,14,40,47]) == [5,8,11]
-    assert get_longest_arithmetic_progression([11,13,15,17,21,25,26]) == [11,13,15,17]
-
 
 '''
 10. Toate numerele sunt pare.
 '''
 
 
-def get_all_even(lst):
-    for i in range(1, len(lst)):
-        if lst[i] % 2 == 0:
-            return True
-        return False
+def este_par(n):
+    '''
+    Verifica daca un numar este par
+    :return:True daca numarul este par, False in caz contrar
+    '''
+    if n%2 == 0:
+        return True
+    return False
+
+
+def toate_sunt_pare(lst):
+    '''
+    Verifica daca toate elementele din lista sunt pare
+    :param lst: lista
+    :return: True, daca toate elementele sunt pare, False in caz contrar
+    '''
+    for x in lst:
+        if este_par(int(x)) is False:
+            return False
+    return True
+
+
+def test_toate_sunt_pare():
+    assert toate_sunt_pare([2,4,6,8]) is True
+    assert toate_sunt_pare([1,3,6,3]) is False
+    assert toate_sunt_pare([1,3]) is False
 
 
 def get_longest_all_even(lst):
-    subsecvpare=[]
-    for i in range (len(lst)):
-        for j in range (i, len(lst)):
-            if get_all_even(lst[i:j+1]) and len(lst[i:j+1]) > len(subsecvpare):
-                subsecvpare=lst[i:j+1]
-    return subsecvpare
-    pass
+    lst_max = []
+    for i in range(0,len(lst)):
+        for j in range(i,len(lst)):
+            if toate_sunt_pare(lst[i:j+1]) and (len(lst[i:j+1]) > len(lst_max)):
+                lst_max = lst[i:j+1]
+    return lst_max
+
 
 def test_get_longest_all_even():
-    assert get_longest_all_even([1,2,3,4,5,6,8,10])==[6,8,10]
-    assert get_longest_all_even([10,12,14,16,18,19,21])==[10,12,14,16,18]
-    assert get_longest_all_even([22,23,24,25,26,28])==[26,28]
+    assert (get_longest_all_even([2,4,5]) == [2,4]) is True
+    assert (get_longest_all_even([1,2,3,5,4]) == [2,4]) is False
 
 
 def main():
@@ -121,7 +144,8 @@ def main():
 
 
 if __name__ == "__main__":
-    test_get_longest_arithmetic_progression()
+    test_get_longest_all_even()
+    test_get_longest_all_prime()
 
 main()
 exit(0)
